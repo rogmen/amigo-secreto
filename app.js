15b5130b609c3bf0179f96e1e109b67287cb7200
rogmen <rogeriommellos@gmail.com>
2025-08-02 18:51:44 -0300

    iniciando

---
+++ app.js
@@ -0,0 +1,17 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let contador = 1;
+
+let lista = [];
+let listaAmigos = document.getElementById("listaAmigos");
+let resultado = document.getElementById("resultado");
+
+
+function adicionar(){
+
+}
+
+function adicionarAmigo(amigo) {
+
+function sortearAmigo(){
+
+}
